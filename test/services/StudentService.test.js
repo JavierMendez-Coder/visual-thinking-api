dfdb75d755337d87b907e0865681ffd042ba5b3c
Javier Méndez <javiermendez0299@gmail.com>
2022-04-26 14:08:06 -0700

    Update StudentService file and tests
    Update getCertificatedStudets() method to return a list of emails instead of objects; Update StudentService.test.js to remove one test case

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -11,13 +11,6 @@ describe("Unit Tests for the StudentService class", () => {
     expect(certificatedStudents).not.toBeUndefined();
   });
 
-  test("Test getCertificatedStudents() method to return students with only a certification", () => {
-    const certificatedStudents = StudentService.getCertificatedStudents();
-    const areAllCertificated = certificatedStudents.every((student) => student.haveCertification);
-
-    expect(areAllCertificated).toBeTruthy();
-  });
-
   test("Test getStudentsOver500Credits() method to not be undefined", () => {
     const studentsOver500Credits = StudentService.getStudentsOver500Credits();
     expect(studentsOver500Credits).not.toBeUndefined();
@@ -25,7 +18,9 @@ describe("Unit Tests for the StudentService class", () => {
 
   test("Test getStudentsOver500Credits() method to return students with credits greater than 500", () => {
     const studentsOver500Credits = StudentService.getStudentsOver500Credits();
-    const areAllOver500Credits  = studentsOver500Credits.every((student) => student.credits > 500);
+    const areAllOver500Credits = studentsOver500Credits.every(
+      (student) => student.credits > 500
+    );
 
     expect(areAllOver500Credits).toBeTruthy();
   });
