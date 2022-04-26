f8354c2fd23abbf306f68d60e07251cb81bab85a
Javier Méndez <javiermendez0299@gmail.com>
2022-04-26 13:22:25 -0700

    Update StudentService to import Reader

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,33 +1,18 @@
 const StudentService = require("./../../lib/services/StudentService");
 
 describe("Unit Tests for the StudentService class", () => {
-  test("Test getCertificatedStudents() method", () => {
-    const students = [
-      { haveCertification: false },
-      { haveCertification: true },
-      { haveCertification: false },
-      { haveCertification: true },
-      { haveCertification: true },
-    ];
-    const certificatedStudents =
-      StudentService.getCertificatedStudents(students);
+  test("Test getStudents() method", () => {
+    const students = StudentService.getStudents();
+    expect(students).not.toBeUndefined();
+  });
 
+  test("Test getCertificatedStudents() method", () => {
+    const certificatedStudents = StudentService.getCertificatedStudents();
     expect(certificatedStudents).not.toBeUndefined();
-    expect(certificatedStudents.length).toBe(3);
   });
 
   test("Test getStudentsOver500Credits() method", () => {
-    const students = [
-      { credits: 550 },
-      { credits: 435 },
-      { credits: 740 },
-      { credits: 365 },
-      { credits: 650 },
-    ];
-    const certificatedStudents =
-      StudentService.getStudentsOver500Credits(students);
-
-    expect(certificatedStudents).not.toBeUndefined();
-    expect(certificatedStudents.length).toBe(3);
+    const studentsOver500Credits = StudentService.getStudentsOver500Credits();
+    expect(studentsOver500Credits).not.toBeUndefined();
   });
 });
